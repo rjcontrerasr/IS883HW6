b1feba082cd94c485c27a7308886daf9163eef99
rjcontrerasr <156374431+rjcontrerasr@users.noreply.github.com>
2024-10-19 21:59:38 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,20 @@
 import streamlit as st
+from transformers import pipeline
+import os
 
-st.title("🎈 My new app")
+st.title("IS883_Homework6")
+
+prompt = st.text_input("Enter a prompt", "Today")
+
+
+### Create a GPT2 generator pipeline
+generator = pipeline('text-generation', model='gpt2')
+
+### Generate the answer to the question "Enter your prompt"
+response = generator(prompt, max_length=20, num_return_sequences=10, truncation=True)
+
+
+### Display
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    response[0]['generated_text']
 )
